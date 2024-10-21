0d34bc6f7d907bcb75a8557cd884c0b7592cce62
yike5460 <yike5460@users.noreply.github.com>
2024-10-21 14:59:55 +0000

    deploy: 48e1d0ce99aedcac310f4a4a758fc377e87f00f0

---
+++ _next/static/a9TAcjaKY4NaeE_SWZkHo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-424a21e3175456f2.js"],"/404":["static/chunks/pages/404-cd59ea9be919ff1b.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
